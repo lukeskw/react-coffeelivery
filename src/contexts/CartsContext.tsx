e2c5c39366e7865297216da0f3ae56e36b8e3d71
lukeskw <lucasporfirioa@gmail.com>
2024-04-30 18:20:58 -0300

    refactor: refactoring cart context

--- src/contexts/CartsContext.tsx
+++ src/contexts/CartsContext.tsx
@@ -1,4 +1,4 @@
-import {
+import React, {
   ReactNode,
   useCallback,
   useState,
@@ -8,51 +8,27 @@ import {
 import { useNavigate } from 'react-router-dom'
 import { OrderInfo } from '../pages/Cart'
 
-export interface Cart {
-  id: string
-  // title: string
-  // description: string
-  // tags: string[]
-  // price: number
-  // image: string
-  quantity: number
-}
-
-export interface Item {
+interface CartItem {
   id: string
   quantity: number
 }
 
-// export interface Order extends OrderInfo {
-//   id: number
-//   items: Item[]
-// }
+interface OrderItem extends OrderInfo {
+  id: string
+  quantity: number
+}
 
-// interface CartState {
-//   cart: Item[]
-//   orders: Order[]
-// }
-
-export interface Order {
+interface Order {
   id: string
   order: OrderInfo
-  items: {
-    quantity: number
-    id: string
-    title: string
-    description: string
-    tags: string[]
-    price: number
-    image: string
-  }[]
+  items: OrderItem[]
   totalPrice: number
 }
 
 interface CartContextType {
-  carts: Item[]
+  carts: CartItem[]
   orders: Order | undefined
-  // setCarts: (carts: Cart[]) => void
-  addItem: (item: Item) => void
+  addItem: (item: CartItem) => void
   checkout: (order: Order) => void
   incrementItemQuantity: (id: string) => void
   decrementItemQuantity: (id: string) => void
@@ -66,17 +42,12 @@ interface CartsProviderProps {
 export const CartsContext = createContext({} as CartContextType)
 
 export function CartsProvider({ children }: CartsProviderProps) {
-  const [orders, setOrders] = useState<Order>()
-  const [carts, setCarts] = useState<Item[]>(() => {
+  const [orders, setOrders] = useState<Order | undefined>()
+  const [carts, setCarts] = useState<CartItem[]>(() => {
     const storedStateAsJSON = localStorage.getItem(
       '@coffee-delivery:cart-state-1.0.0',
     )
-
-    if (storedStateAsJSON) {
-      return JSON.parse(storedStateAsJSON)
-    }
-
-    return []
+    return storedStateAsJSON ? JSON.parse(storedStateAsJSON) : []
   })
 
   const navigate = useNavigate()
@@ -92,54 +63,58 @@ export function CartsProvider({ children }: CartsProviderProps) {
     [navigate],
   )
 
-  function addItem(item: Item) {
-    console.log('Add Item:', item, carts)
-    const itemAlreadyAdded = carts.find((i) => item.id === i.id)
+  const addItem = useCallback(
+    (item: CartItem) => {
+      const itemIndex = carts.findIndex((cartItem) => cartItem.id === item.id)
+      if (itemIndex !== -1) {
+        const updatedCarts = [...carts]
+        updatedCarts[itemIndex] = {
+          ...updatedCarts[itemIndex],
+          quantity: item.quantity,
+        }
+        setCarts(updatedCarts)
+      } else {
+        setCarts((prevCarts) => [...prevCarts, item])
+      }
+    },
+    [carts],
+  )
 
-    console.log(itemAlreadyAdded, 'item already added')
-    if (itemAlreadyAdded) {
-      console.log(itemAlreadyAdded.quantity, item.quantity)
-      itemAlreadyAdded.quantity = item.quantity
-    } else {
-      setCarts([...carts, item])
-    }
-    return carts
-  }
-  function incrementItemQuantity(id: string) {
-    setCarts((state) =>
-      state.map((item) => {
+  const updateItemQuantity = useCallback((id: string, delta: number) => {
+    setCarts((prevCarts) =>
+      prevCarts.map((item) => {
         if (item.id === id) {
-          return { ...item, quantity: item.quantity + 1 }
+          const updatedQuantity = item.quantity + delta
+          return { ...item, quantity: Math.max(updatedQuantity, 1) }
         }
         return item
       }),
     )
-  }
-  function decrementItemQuantity(id: string) {
-    setCarts((state) =>
-      state.map((item) => {
-        if (item.id === id) {
-          if (item.quantity > 1) {
-            return {
-              ...item,
-              quantity: item.quantity - 1,
-            }
-          }
-        }
-        return item
-      }),
-    )
-  }
-  function removeItem(id: string) {
-    setCarts((state) => state.filter((item) => item.id !== id))
-  }
+  }, [])
+
+  const incrementItemQuantity = useCallback(
+    (id: string) => {
+      updateItemQuantity(id, 1)
+    },
+    [updateItemQuantity],
+  )
+
+  const decrementItemQuantity = useCallback(
+    (id: string) => {
+      updateItemQuantity(id, -1)
+    },
+    [updateItemQuantity],
+  )
+
+  const removeItem = useCallback((id: string) => {
+    setCarts((prevCarts) => prevCarts.filter((item) => item.id !== id))
+  }, [])
 
   useEffect(() => {
-    if (carts) {
-      const stateJSON = JSON.stringify(carts)
-
-      localStorage.setItem('@coffee-delivery:cart-state-1.0.0', stateJSON)
-    }
+    localStorage.setItem(
+      '@coffee-delivery:cart-state-1.0.0',
+      JSON.stringify(carts),
+    )
   }, [carts])
 
   return (
